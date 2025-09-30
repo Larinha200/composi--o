a5e3f5e4e9fe425de4bbd695fda13adfd02948cc
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-30 08:05:13 -0300

    classes

--- classess.py
+++ classess.py
@@ -1,11 +1,23 @@
 class Processador:
-    pass
+    def __init__(self, modelo, velocidade_ghz):
+        self.__modelo = modelo
+        self.__velocidade_ghz = velocidade_ghz
 
 class Memoria_RAM:
-    pass
+    def __init__(self,capacidade_gb, tipo):
+        self.__capacidade_gb = capacidade_gb
+        self.__tipo = tipo
 
 class Armazenamento:
-    pass
+    def __init__(self, tipo, capacidade_gb):
+        self.__tipo = tipo
+        self.__capacidade_gb = capacidade_gb
 
 class Computador:
-    pass
+    def __init__(self, marca, modelo):
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__processador = Processador()
+        self.__memoria_RAM = Memoria_RAM()
+        self.__armazenamento = Armazenamento()
+        
