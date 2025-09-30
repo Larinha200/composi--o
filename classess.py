2df7056c47e415372eb6d492708d536b20d37bd1
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-30 08:11:44 -0300

    classes

--- classess.py
+++ classess.py
@@ -2,16 +2,35 @@ class Processador:
     def __init__(self, modelo, velocidade_ghz):
         self.__modelo = modelo
         self.__velocidade_ghz = velocidade_ghz
+    
+    def getModelo(self):
+        return self.__modelo 
+    
+    def getVelocidade_ghz(self):
+        return self.__velocidade_ghz 
+
 
 class Memoria_RAM:
     def __init__(self,capacidade_gb, tipo):
         self.__capacidade_gb = capacidade_gb
         self.__tipo = tipo
 
+    def getCapacidade_gb(self):
+        return self.__capacidade_gb
+    
+    def getTipo(self):
+        return self.__tipo
+    
 class Armazenamento:
     def __init__(self, tipo, capacidade_gb):
         self.__tipo = tipo
         self.__capacidade_gb = capacidade_gb
+
+    def getTipo(self):
+        return self.__tipo
+    
+    def getCapacidade_gb(self):
+        return self.__capacidade_gb
 
 class Computador:
     def __init__(self, marca, modelo):
@@ -20,4 +39,12 @@ class Computador:
         self.__processador = Processador()
         self.__memoria_RAM = Memoria_RAM()
         self.__armazenamento = Armazenamento()
-        
+
+    def getMarca(self):
+        return self.__marca
+    
+    def getModelo(self):
+        return self.__modelo
+    
+    def ligar ():
+        pass
