e8fbd2b3be8ddff7543db5475f91dac85c84a442
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-30 09:46:55 -0300

    .

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from classess import*
 
-computador = Computador()
 
+pro1= Processador('a', 10)
+pro2= Processador('b', 20)
+pro3= Processador('c', 30)
+pro4= Processador('d', 40)
 
+mem1= Memoria_RAM(20,'static')
+mem2= Memoria_RAM(10, 'dynamic')
+
+arma1= Armazenamento('SSD',10)
+arma2= Armazenamento('HDD',20)
+
+com1= Computador('del', '1', 'a', 10 , 'static','SSD',40)
+com2= Computador('del', '1', 'b',20,'dynamic', 'SSD',30)
+com3= Computador('del', '1', 'c',30, 'static', 'HDD',20)
+com4= Computador('del', '1', 'd',40, 'dynamic', 'HDD',10)
+
+resp= int(input("--->"))
 match resp:
     case 1:
-        pass
+        print(com1)
--- classess.py
+++ classess.py
@@ -1,53 +1,53 @@
 class Processador:
-    def __init__(self, modelo, velocidade_ghz):
-        self.__modelo = modelo
-        self.__velocidade_ghz = velocidade_ghz
+    def __init__(self, modelo_pro, velocidade_ghz_pro):
+        self.__modelo_pro = modelo_pro
+        self.__velocidade_ghz_pro = velocidade_ghz_pro
     
-    def getModelo(self):
-        return self.__modelo 
+    def getModelo_pro(self):
+        return self.__modelo_pro
     
-    def getVelocidade_ghz(self):
-        return self.__velocidade_ghz 
+    def getVelocidade_ghz_pro(self):
+        return self.__velocidade_ghz_pro 
     
     def __str__(self):
-        return f'Modelo:{self.__modelo} \nVelocidade ghz:{self.__velocidade_ghz }'
+        return f'Modelo:{self.__modelo_pro} \nVelocidade ghz:{self.__velocidade_ghz_pro }'
 
 
 class Memoria_RAM:
-    def __init__(self,capacidade_gb, tipo):
-        self.__capacidade_gb = capacidade_gb
-        self.__tipo = tipo
+    def __init__(self,capacidade_gb_RAM, tipo_RAM):
+        self.__capacidade_gb_RAM = capacidade_gb_RAM
+        self.__tipo_RAM = tipo_RAM
 
     def getCapacidade_gb(self):
-        return self.__capacidade_gb
+        return self.__capacidade_gb_RAM
     
     def getTipo(self):
-        return self.__tipo
+        return self.__tipo_RAM
     
     def __str__(self):
-        return f'Capacidade gb: {self.__capacidade_gb} \nTipo: {self.__tipo}'
+        return f'Capacidade gb: {self.__capacidade_gb_RAM} \nTipo: {self.__tipo_RAM}'
     
 class Armazenamento:
-    def __init__(self, tipo, capacidade_gb):
-        self.__tipo = tipo
-        self.__capacidade_gb = capacidade_gb
+    def __init__(self, tipo_ar, capacidade_gb_ar):
+        self.__tipo_ar = tipo_ar
+        self.__capacidade_gb_ar = capacidade_gb_ar
 
-    def getTipo(self):
-        return self.__tipo
+    def getTipo_ar(self):
+        return self.__tipo_ar
     
-    def getCapacidade_gb(self):
-        return self.__capacidade_gb
+    def getCapacidade_gb_ar(self):
+        return self.__capacidade_gb_ar
     
     def __str__(self):
-        return f'Tipo: {self.__tipo} \nCapacidade gb: {self.__capacidade_gb}'
+        return f'Tipo: {self.__tipo_ar} \nCapacidade gb: {self.__capacidade_gb_ar}'
     
 class Computador:
-    def __init__(self, marca, modelo):
+    def __init__(self, marca, modelo,modelo_pro,velocidade_ghz_pro,capacidade_gb_RAM,tipo_RAM,tipo_ar, capacidade_gb_ar):
         self.__marca = marca
         self.__modelo = modelo
-        self.__processador = Processador()
-        self.__memoria_RAM = Memoria_RAM()
-        self.__armazenamento = Armazenamento()
+        self.__processador = Processador(modelo_pro, velocidade_ghz_pro)
+        self.__memoria_RAM = Memoria_RAM(capacidade_gb_RAM , tipo_RAM)
+        self.__armazenamento = Armazenamento(tipo_ar, capacidade_gb_ar)
 
     def getMarca(self):
         return self.__marca
@@ -56,7 +56,7 @@ class Computador:
         return self.__modelo
     
     def __str__(self):
-        return f'Marca: {self.__marca} \nModelo: {self.__modelo} \nProcessador: \nModelo: {self.__processador.getModelo} \nVelocidade ghz: {self.__processador.getVelocidade_ghz} \nMemoria RAM: \nCpacidade gb:{self.__memoria_RAM.getCapacidade_gb} \ntipo: {self.__memoria_RAM.getTipo} \nArmezenamento: \nCapacidade gb: {self.__armazenamento.getCapacidade_gb} \nTipo: {self.__armazenamento.getTipo}'
+        return f'Marca: {self.__marca} \nModelo: {self.__modelo} \nProcessador: \nModelo: {self.__processador.getModelo_pro} \nVelocidade ghz: {self.__processador.getVelocidade_ghz} \nMemoria RAM: \nCpacidade gb:{self.__memoria_RAM.getCapacidade_gb} \ntipo: {self.__memoria_RAM.getTipo} \nArmezenamento: \nCapacidade gb: {self.__armazenamento.getCapacidade_gb} \nTipo:{self.__armazenamento.getTipo}'
     
     
     def ligar (self):
